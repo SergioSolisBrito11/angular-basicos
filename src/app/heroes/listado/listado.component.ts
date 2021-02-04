3864ca51db6b645633aefc781462ba32d288b970
Sergio Solis Brito <sergio.solis@ids.com.mx>
2021-02-03 18:14:56 -0600

    hola

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
 
-  heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor', 'Capitán América'];
+  heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor', 'Capitán América',"Superman"];
   heroeBorrado: string = '';
 
   borrarHeroe(){
